1b8f5c2e1a5bce565165c7c8e8dbfb5c8c9c4eae
Ewa Olczak <ewa.olczak@gmail.com>
2023-05-07 22:56:47 +0200

    Fix error, delete commented code

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const ObjectId = require('mongodb').ObjectId;
 
 router.get('/departments', (req, res) => {
-  // res.json(db.departments);
   req.db
     .collection('departments')
     .find()
@@ -14,7 +13,6 @@ router.get('/departments', (req, res) => {
 });
 
 router.get('/departments/random', (req, res) => {
-  // res.json(db.departments[Math.floor(Math.random() * db.length)]);
   req.db
     .collection('departments')
     .aggregate([{ $sample: { size: 1 } }])
@@ -25,7 +23,6 @@ router.get('/departments/random', (req, res) => {
 });
 
 router.get('/departments/:id', (req, res) => {
-  // res.json(db.departments.find((item) => item.id == req.params.id));
   req.db
     .collection('departments')
     .findOne({ _id: ObjectId(req.params.id) }, (err, data) => {
@@ -37,7 +34,6 @@ router.get('/departments/:id', (req, res) => {
 
 router.post('/departments', (req, res) => {
   const { name } = req.body;
-  // db.departments.push({ id: 3, name });
   req.db.collection('departments').insertOne({ name: name }, (err) => {
     if (err) res.status(500).json({ message: err });
     else res.json({ message: 'OK' });
@@ -46,9 +42,6 @@ router.post('/departments', (req, res) => {
 
 router.put('/departments/:id', (req, res) => {
   const { name } = req.body;
-  /* db = db.departments.map((item) =>
-    item.id == req.params.id ? { ...item, name } : item
-  ); */
   req.db
     .collection('departments')
     .updateOne(
@@ -62,12 +55,11 @@ router.put('/departments/:id', (req, res) => {
 });
 
 router.delete('/departments/:id', (req, res) => {
-  // db = db.departments.filter((item) => item.id != req.params.id);
   req.db
     .collection('departments')
     .deleteOne({ _id: ObjectId(req.params.id) }, (err) => {
       if (err) res.status(500).json({ message: err });
-      res.json({ message: 'OK' });
+      else res.json({ message: 'OK' });
     });
 });
 
