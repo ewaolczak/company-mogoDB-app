106982492c78e7995ef87951f12719b5041cf8e9
Ewa Olczak <ewa.olczak@gmail.com>
2023-05-14 12:03:39 +0200

    Add employees polulate and add departments controllers

--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -4,7 +4,7 @@ const Employee = require('../models/employee.model');
 
 router.get('/employees', async (req, res) => {
   try {
-    res.json(await Employee.find());
+    res.json(await Employee.find().populate('department'));
   } catch (err) {
     res.status(500).json({ message: err });
   }
@@ -14,7 +14,7 @@ router.get('/employees/random', async (req, res) => {
   try {
     const count = await Employee.countDocuments();
     const rand = Math.floor(Math.random() * count);
-    const empl = await Employee.findOne().skip(rand);
+    const empl = await Employee.findOne().populate('department').skip(rand);
     if (!empl) res.status(404).json({ message: 'Not found' });
     else res.json(empl);
   } catch (err) {
@@ -24,7 +24,7 @@ router.get('/employees/random', async (req, res) => {
 
 router.get('/employees/:id', async (req, res) => {
   try {
-    const empl = await Employee.findById(req.params.id);
+    const empl = await Employee.findById(req.params.id).populate('department');
     if (!empl) res.status(404).json({ message: 'Not found' });
     else res.json(empl);
   } catch (err) {
