543684c46ed4d25a7e55bfabf7e9958d352252a4
Ewa Olczak <ewa.olczak@gmail.com>
2023-05-20 11:20:19 +0200

    prepare get.test.js

--- test/api/departments/get.test.js
+++ test/api/departments/get.test.js
@@ -1,5 +1,15 @@
-const expect = require('chai').expect
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+
+const expect = chai.expect;
+const request = chai.request;
 
 describe('GET /api/departments', () => {
-  
-})
+  it('/ should return all departments', () => {});
+
+  it('/:id should return one department by :id ', () => {});
+
+  it('/random should return one random department', () => {});
+});
